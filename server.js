547f10ca34fa764ec75c1e409193e620c1f0fc74
Aacgectyuoki <maximiliandellthib@mail.adelphi.edu>
2021-04-13 15:27:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var server = http.createServer(app);
 var io = require('socket.io')(server);
 var path = require('path');
 
-// 
+// access "index.html"
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
